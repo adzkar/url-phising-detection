1eb729387010cc2412098096d7ec07ebc2c3dcc4
Adzkar Fauzie <adzkarfauzie02@gmail.com>
2021-01-10 13:23:10 +0700

    feat: change svm to knn

--- knn.py
+++ knn.py
@@ -1,6 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 import numpy as np 
 import pandas as pd
 
@@ -18,12 +19,13 @@ Y_train = training_set.iloc[:,11].values
 X_test = test_set.iloc[:,1:11].values
 Y_test = test_set.iloc[:,11].values
 
-classifier = SVC(kernel='rbf', random_state = 1,C=10)
+classifier = KNeighborsClassifier(n_neighbors=3)
 classifier.fit(X_train,Y_train)
 
 Y_pred = classifier.predict(X_test)
 test_set["Predictions"] = Y_pred
 
+print('Hasil Learning')
 print(confusion_matrix(Y_test,Y_pred.round()))
 print(classification_report(Y_test,Y_pred.round()))
 print(accuracy_score(Y_test, Y_pred.round()))
